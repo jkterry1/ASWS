145c3d5f40620b159fc481e0f6145c23d5c7d84e
Mario Jayakumar <mario.jayakumar@gmail.com>
2021-04-17 16:12:44 -0400

    fixed minor typos

--- pytorch-training/imagenettrain.py
+++ pytorch-training/imagenettrain.py
@@ -68,7 +68,7 @@ parser.add_argument('--dist-backend', default='nccl', type=str,
                     help='distributed backend')
 parser.add_argument('--seed', default=None, type=int,
                     help='seed for initializing training. ')
-parser.add_argument('--gpu', default=None, type=int,
+parser.add_argument('--gpu', default=0, type=int,
                     help='GPU id to use.')
 parser.add_argument('--multiprocessing-distributed', action='store_true',
                     help='Use multi-processing distributed training to launch '
@@ -240,7 +240,7 @@ def main_worker(gpu, ngpus_per_node, args):
 
     run_num = args.run
     model_name = args.arch
-    outputfilename = str(model_name) + "_imagenetrun_" + run_num + ".txt"
+    outputfilename = str(model_name) + "_imagenetrun_" + str(run_num) + ".txt"
 
     outfh = open(outputfilename, "w+")
 
